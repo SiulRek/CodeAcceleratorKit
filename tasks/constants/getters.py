292add6bf3627499c608dc075277ad20a0e7d82d
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-13 18:27:30 +0200

    Added get_task_cache_directory, get_temporary_script_path.

--- tasks/constants/getters.py
+++ tasks/constants/getters.py
@@ -7,10 +7,11 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 TASKS_CODE_ROOT = os.path.join(FILE_DIR, "..", "..")
 
 
-def get_task_cache_directory():
-    dir = os.path.join(TASKS_CODE_ROOT, "tasks",  "__taskcache__")
+def get_task_cache_directory(name):
+    dir = os.path.join(TASKS_CODE_ROOT, "tasks",  "__taskcache__", name)
     os.makedirs(dir, exist_ok=True)
     return os.path.normpath(dir)
+
 
 def get_response_file_path(root_dir):
     dir = os.path.join(root_dir, "local", "tasks_room", "outputs")
@@ -18,7 +19,7 @@ def get_response_file_path(root_dir):
     path = os.path.join(dir, "response.txt")
     return os.path.normpath(path)
 
-
+#TODO change to query file path
 def get_temporary_file_path(root_dir):
     dir = os.path.join(root_dir, "local", "tasks_room", "outputs")
     os.makedirs(dir, exist_ok=True)
@@ -36,6 +37,18 @@ def get_query_templates_directory(root_dir):
     dir = os.path.join(root_dir, "local", "tasks_room", "data", "query_templates")
     os.makedirs(dir, exist_ok=True)
     return os.path.normpath(dir)
+
+
+def get_temporary_script_path(root_dir):
+    if not hasattr(get_temporary_script_path, "counter"):
+        get_temporary_script_path.counter = 1
+    else:
+        get_temporary_script_path.counter += 1
+    name = f"script_{get_temporary_script_path.counter}.py"
+    dir = os.path.join(root_dir, "local", "tasks_room", "temp")
+    os.makedirs(dir, exist_ok=True)
+    path = os.path.join(dir, name)
+    return os.path.normpath(path)
 
 
 def get_output_directory(root_dir):
