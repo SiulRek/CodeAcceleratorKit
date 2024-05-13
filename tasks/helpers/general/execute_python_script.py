5ae70a64dd563a47aa0cca116851e468386dfbfc
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-13 18:28:30 +0200

    Added features to execute_python_script function.

--- tasks/helpers/general/execute_python_script.py
+++ tasks/helpers/general/execute_python_script.py
@@ -1,29 +1,60 @@
 import os
+import sys
+import shutil
 import subprocess
 
 
-def execute_python_script(script_path, env_python_path):
+def execute_python_script(script, env_python_path, cwd=None, temp_script_path=None, args=None):
     """
-    Executes a Python script located at the specified path using the Python
-    interpreter from the virtual environment and captures its output.
+    Executes a specified Python script using a designated Python interpreter from a virtual environment,
+    potentially in a different working directory or a temporary location. This function supports passing 
+    additional arguments to the script. It captures and returns the script's standard output. If an error 
+    occurs during the script's execution, it captures and returns the error output.
 
     Args:
-        - script_path (str): The absolute path to the Python script to
-            execute.
+        - script (str): The path to or the module of the Python script to run.
         - env_python_path (str): The path to the Python interpreter in the
             virtual environment.
+        - cwd (str): The current working directory for the script execution.
+            Default is None.
+        - temp_script_path (str): The path to copy the script to before. Useful
+            when the script is located in a different workspace. Default is None.
+        - args (list): The list of arguments to pass to the script. Default is None.
 
     Returns:
         - str: The output from the script execution or an error message if
             execution fails.
     """
-    python_path = os.path.join(env_python_path, "bin", "python")
+    if hasattr(script, "__file__"):
+        script = script.__file__
+    
+    if temp_script_path:
+        shutil.copy(script, temp_script_path)
+        script = temp_script_path
+        
+    if sys.platform == "win32":
+        python_path = os.path.join(env_python_path, "Scripts", "python")
+    else:
+        python_path = os.path.join(env_python_path, "bin", "python")
+
     try:
-        completed_process = subprocess.run(
-            [python_path, "-u", script_path],
-            capture_output=True,
-            text=True,
-            check=True,
+        command = [python_path, "-u", script]
+        if args:
+            command.extend(args)
+        if cwd:
+            completed_process = subprocess.run(
+                command,
+                cwd=cwd,
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+        else:
+            completed_process = subprocess.run(
+                command,
+                capture_output=True,
+                text=True,
+                check=True,
         )
         return completed_process.stdout
     except subprocess.CalledProcessError as e:
