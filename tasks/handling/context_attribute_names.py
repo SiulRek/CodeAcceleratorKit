6c276e947c2617fd80a9737413a66888ed762ae1
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-19 15:47:11 +0200

    Add description to directory paths.

--- tasks/handling/context_attribute_names.py
+++ tasks/handling/context_attribute_names.py
@@ -3,5 +3,7 @@ from enum import Enum
 
 class ContextAttrNames(Enum):
     """Enumeration for context attribute names."""
+    # All directory path attributes should end with "_dir"
+    # The dirs should be placed first in the enumeration
     cwd = "cwd"
     python_env = "python_env"
