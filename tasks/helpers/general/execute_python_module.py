e04857be37a7bae98a695648e7dcd112490d51a1
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-19 12:02:21 +0200

    Rename execute_python_script to execute_python_module

--- tasks/helpers/general/execute_python_module.py
+++ tasks/helpers/general/execute_python_module.py
@@ -4,9 +4,9 @@ import shutil
 import subprocess
 
 
-def execute_python_script(script, env_python_path, cwd=None, temp_script_path=None, args=None):
+def execute_python_module(module, env_python_path, cwd=None, temp_script_path=None, args=None):
     """
-    Executes a specified Python script using a designated Python interpreter from a virtual environment,
+    Executes a specified Python module using a designated Python interpreter from a virtual environment,
     potentially in a different working directory or a temporary location. This function supports passing 
     additional arguments to the script. It captures and returns the script's standard output. If an error 
     occurs during the script's execution, it captures and returns the error output.
@@ -25,12 +25,12 @@ def execute_python_script(script, env_python_path, cwd=None, temp_script_path=No
         - str: The output from the script execution or an error message if
             execution fails.
     """
-    if hasattr(script, "__file__"):
-        script = script.__file__
+    if hasattr(module, "__file__"):
+        module = module.__file__
     
     if temp_script_path:
-        shutil.copy(script, temp_script_path)
-        script = temp_script_path
+        shutil.copy(module, temp_script_path)
+        module = temp_script_path
         
     if sys.platform == "win32":
         python_path = os.path.join(env_python_path, "Scripts", "python")
@@ -38,7 +38,7 @@ def execute_python_script(script, env_python_path, cwd=None, temp_script_path=No
         python_path = os.path.join(env_python_path, "bin", "python")
 
     try:
-        command = [python_path, "-u", script]
+        command = [python_path, "-u", module]
         if args:
             command.extend(args)
         if cwd:
@@ -66,6 +66,6 @@ def execute_python_script(script, env_python_path, cwd=None, temp_script_path=No
 if __name__ == "__main__":
     script_path = "/path/to/python/script.py"
     env_python_path = "path/to/python/interpreter"
-    script_output = execute_python_script(script_path, env_python_path)
+    script_output = execute_python_module(script_path, env_python_path)
     with open("output.txt", "w") as output_file:
         output_file.write(script_output)
