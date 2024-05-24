0ebc5033cb3a983cd7e85587cbe21a64c92c20d1
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-24 16:44:40 +0200

    Fix issue with end text

--- tasks/tools/for_create_query/add_text_tags.py
+++ tasks/tools/for_create_query/add_text_tags.py
@@ -13,6 +13,6 @@ def add_text_tags(begin_text, end_text, text):
     stop_sep = "\n\n" + "*" * 10 + "\n\n"
     start_sep = "\n\n" + "*" * 10 + "\n\n"
     begin_text = f"{begin_text}{start_sep}" if begin_text else ""
-    end_text = f"{end_text}{stop_sep}" if end_text else ""
+    end_text = f"{stop_sep}{end_text}" if end_text else ""
     text = f"{begin_text}{text}{end_text}"
     return text
