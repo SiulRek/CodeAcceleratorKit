b357c948d31dbf593387684edb01dd542a650574
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-25 14:10:29 +0200

    Prettify

--- tasks/control/update_all_runners.py
+++ tasks/control/update_all_runners.py
@@ -3,6 +3,5 @@ from tasks.tasks.management.task_manager import TaskManager
 if __name__ == "__main__":
     task_manager = TaskManager()
 
-    task_manager.update_registered_runners(
-        # runner_root="/path/to/runner",
-    )
+    task_manager.update_registered_runners()
+    # Do not forget to to update UPDATE_MAPPING in tasks/configs/session_attributes.py
