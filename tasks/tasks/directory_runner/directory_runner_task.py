c9ff8c48704482d607606f57166f3da81c8bd1e5
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-28 09:22:25 +0200

    Integrated logging of subtask outputs

--- tasks/tasks/directory_runner/directory_runner_task.py
+++ tasks/tasks/directory_runner/directory_runner_task.py
@@ -1,17 +1,16 @@
-from contextlib import contextmanager
 import json
 import os
-import sys
 
 from tasks.tasks.cleanup.cleanup_task import CleanupTask
 from tasks.tasks.create_query.create_query_task import CreateQueryTask
 from tasks.tasks.foundation.task_base import TaskBase
 from tasks.tools.for_directory_runner.file_execution_tracker import FileExecutionTracker
 from tasks.tools.general.backup_handler import BackupHandler
+from tasks.tools.general.log_outputs_to_file import log_outputs_to_file
 
 
 class DirectoryRunnerTask(TaskBase):
-    """A task for running tasks on a directory of files."""
+    """ A task for running tasks on a directory of files. """
 
     NAME = "Directory Runner"
 
@@ -43,21 +42,10 @@ class DirectoryRunnerTask(TaskBase):
     def _get_task_class(self, task_name):
         if task_name == CleanupTask.NAME:
             return CleanupTask
-        elif task_name == CreateQueryTask.NAME:
+        if task_name == CreateQueryTask.NAME:
             return CreateQueryTask
-        else:
-            msg = f"Task {task_name} is not supported."
-            raise ValueError(msg)
-
-    @contextmanager
-    def _suppress_stdout(self):
-        with open(os.devnull, "w") as devnull:
-            old_stdout = sys.stdout
-            sys.stdout = devnull
-            try:
-                yield
-            finally:
-                sys.stdout = old_stdout
+        msg = f"Task {task_name} is not supported."
+        raise ValueError(msg)
 
     def setup(self):
         super().setup()
@@ -69,16 +57,20 @@ class DirectoryRunnerTask(TaskBase):
         self._set_attributes_from_json(self.directory_runner_json)
 
     def execute(self):
-        """Executes the directory runner task, running the specified task on the
-        files in the directory."""
+        """ Executes the directory runner task, running the specified task on
+        thefiles in the directory. """
         execution_tracks_dir = self.profile.execution_tracks_dir
         csv_name = (
             os.path.basename(self.directory_path).split(".")[0]
             + "_execution_tracks.csv"
         )
         file_execution_csv = os.path.join(execution_tracks_dir, csv_name)
-        
-        backup_handler = BackupHandler(self.profile.backup_dir, self.profile.max_backups)
+        output_log_name = csv_name.replace("_execution_tracks.csv", "_output.txt")
+        output_log_file = os.path.join(self.profile.log_dir, output_log_name)
+
+        backup_handler = BackupHandler(
+            self.profile.backup_dir, self.profile.max_backups
+        )
         if self.clear_backup_storage:
             backup_handler.clear_storage()
 
@@ -96,23 +88,27 @@ class DirectoryRunnerTask(TaskBase):
             )
 
         print(f"\nExecution Tracker initialized: {file_execution_csv}")
+        print(f"Output log file: {output_log_file}\n")
         count_status = execution_tracker.get_status_count()
         pendings_at_start = count_status.get("pending")
         print(f"Number of files pending at start: {pendings_at_start}\n")
 
         completed_files = 0
         failed_files = 0
+        append = False
         while True:
             file_path = execution_tracker.take_next_pending()
             if file_path is None:
                 break
 
-            task_class = self._get_task_class(self.task_name)
-            task = task_class(self.task_runner_root, file_path, self.macros_text)
-            task.force_defaults() # Prevents the task from using the command line arguments
+            subtask_class = self._get_task_class(self.task_name)
+            subtask = subtask_class(self.task_runner_root, file_path, self.macros_text)
+            subtask.force_defaults()  # Prevents the task from using the command line arguments
             try:
-                with self._suppress_stdout():
-                    task.main()
+                with log_outputs_to_file(output_log_file, append=append):
+                    append = True
+                    subtask.main()
+                    print(f"\n\n")
                 execution_tracker.mark_running_as_completed()
                 completed_files += 1
             except Exception as e:
