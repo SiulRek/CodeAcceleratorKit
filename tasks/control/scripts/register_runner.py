658d5138e3b52b7b522c97f2760c95b16473f47c
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-05-31 18:37:02 +0200

    Updated register_runner.py script

--- tasks/control/scripts/register_runner.py
+++ tasks/control/scripts/register_runner.py
@@ -2,16 +2,15 @@ from tasks.tasks.management.task_manager import TaskManager
 
 if __name__ == "__main__":
     task_manager = TaskManager()
-
+    runner_root = "/path/to/runner"
     task_manager.register_runner(
-        # runner_root="/path/to/runner",
+        # runner_root=runner_root,
         # python_env="/path/to/runner/python_env",
-        # # storage_dir="/path/to/runner/storage",
-        # # cwd="/path/to/runner/cwd",
-        # # overwrite=True
-        # # create_dirs=True
+        # storage_dir="/path/to/runner/storage",
+        # cwd="/path/to/runner/cwd",
+        # create_dirs=True
     )
     task_manager.load_support_files_to(
-        runner_root="/path/to/runner_root",
+        runner_root=runner_root,
     )
     print("Runner registered successfully.")
