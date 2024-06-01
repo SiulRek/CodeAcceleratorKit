6da427e898e823258f1ed0f342df505886496221
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-01 14:08:04 +0200

    Added descriptions

--- tasks/control/scripts/register_runner.py
+++ tasks/control/scripts/register_runner.py
@@ -5,10 +5,9 @@ if __name__ == "__main__":
     runner_root = "/path/to/runner"
     task_manager.register_runner(
         # runner_root=runner_root,
-        # python_env="/path/to/runner/python_env",
-        # storage_dir="/path/to/runner/storage",
+        # python_env="/path/to/runner/python_env", # e.g. "/path/to/runner/venv"
+        # storage_dir="/path/to/runner/storage",   # In this folder everything the runner needs is going to be stored
         # cwd="/path/to/runner/cwd",
-        # create_dirs=True
     )
     task_manager.load_support_files_to(
         runner_root=runner_root,
