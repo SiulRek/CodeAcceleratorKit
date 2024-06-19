b2392aa5c515fb3e366636c00f7e35f13ecee88b
SiulRek <luis.kraker@edu.fh-joanneum.at>
2024-06-19 10:17:48 +0200

    Update openai library, modified send_prompt to reflect changes.

--- tasks/utils/for_automatic_prompt/send_prompt.py
+++ tasks/utils/for_automatic_prompt/send_prompt.py
@@ -1,35 +1,42 @@
-import openai
+from openai import OpenAI
 
 from keys import OPENAI_KEY
 
 
-def send_prompt(prompt_message, max_response_tokens=3000):
+def send_prompt(prompt_message, max_response_tokens=3000, model="gpt-4o"):
     """
     Sends a prompt to OpenAI's GPT model and returns the response.
 
     Args:
-        prompt_message (str): The message to send to the model.
-        max_response_tokens (int): The maximum number of tokens to generate.
-    
-    Returns:
-        str: The response message from the model.
-    """
-    openai.api_key = OPENAI_KEY
+        - prompt_message (str): The message to send to the model.
+        - max_response_tokens (int): The maximum number of tokens to
+            generate.
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
+    Returns:
+        - str: The response message from the model.
+    """
+    client = OpenAI(api_key=OPENAI_KEY)
+
+    response = client.chat.completions.with_raw_response.create(
         messages=[
-            {"role": "system", "content": "You are a Python and Vision AI developer."},
+            {"role": "system", "content": "You are a Python developer."},
             {"role": "user", "content": prompt_message},
         ],
+        model=model,
         max_tokens=max_response_tokens,
     )
 
-    return response["choices"][0]["message"]["content"]
+
+    completion = (
+        response.parse()
+    )  
+    response_message = completion.choices[0].message.content
+    return response_message
 
 
 if __name__ == "__main__":
     response_message = send_prompt(
-        "Explain unit testing in Python. Tell all you know please"
+        "Explain unit testing in Python. Tell all you know please",
+        max_response_tokens=10,
     )
     print(response_message)
