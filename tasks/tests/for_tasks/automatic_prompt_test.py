6d59b9a36b72bf24eaa27cd01b6d74c8c84cc9f8
SiulRek <luis.kraker@edu.fh-joanneum.at>
2025-04-14 11:21:07 -0500

    Update automating prompt test case

--- tasks/tests/for_tasks/automatic_prompt_test.py
+++ tasks/tests/for_tasks/automatic_prompt_test.py
@@ -28,37 +28,49 @@ class ExampleClassCar:
 #E End of prompt
 #E Merge end text in multiple lines
 
-#T Title of the chapter
+#T Test: Title of the chapter
 
+#T Test: Normal Text (2)
 #N The Normal text
 #N Merge normal texts in multiple lines
 
+#T Test: Paste File (2)
 #P reference_1.py, data\reference_2.py
-#P reference_1.py, data\reference_2.py (True)
 
+#T Test: Paste Current File (2)
 #P File
-#P File (True)
 
+#T Test: Paste Test Errors Log
 #L
 
+#T Test: Fill Text (2)
 #*template_4
 
+#T Test: Meta Macros (2)
 #template_1_meta
 
+#T Test: Meta Macros with Args (2)
 #template_2_meta+ (True, 5)
 
+#T Test: Custom Function (2)
 #template_3_func (True, 5)
 
+#T Test: Run Python Script (2)
 #run data/example_script.py
 
+#T Test: Run Pylint (2)
 #pylint data/example_script.py
 
+#T Test: Run Unittest (2)
 #unittest File (2)
 
+#T Test: Directory Tree (2)
 #tree . (2, False, ['temp', 'log'])
 
+#T Test: Summarize Python Script (2)
 #summarize data/example_script_2.py (True)
 
+#T Test: Summarize Folder (2)
 #summarize_folder data (True, [], ['reference_1.py', 'reference_2.py', 'example_script_6.py'])
 
 # send (False, 100)
